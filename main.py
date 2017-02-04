6ac5d1979881b2cc50d4d7f69695778ca454d1cd
zhanfu shi <1521815135@qq.com>
2017-02-04 18:17:13 +0800

    项目结构初始化

---
+++ main.py
@@ -0,0 +1,52 @@
+#! usr/bin/python
+# coding=utf-8
+
+import os
+import requests
+from bs4 import BeautifulSoup
+
+
+
+def main():
+    originalUrl = 'http://jandan.net/ooxx/page-'
+    rest = req(originalUrl, '1')
+    print(rest)
+    downloadImg(rest)
+
+
+def req(url, pageIndex):
+    resp = requests.get(url + pageIndex)
+    ass = []
+    if resp.status_code == 200:
+        content = resp.content
+        soup = BeautifulSoup(content, "html.parser")
+        links = soup.find_all('a', 'view_img_link')
+        length = len(links)
+        print(length)
+        for i in range(length):
+            ass.append('http:' + links[i].attrs['href'])
+    return ass
+
+
+def downloadImg(urls):
+    index = 0
+    for url in urls:
+        print("下载:", url)
+        # 未能正确获得网页 就进行异常处理
+        try:
+            res = requests.get(url)
+            if res.status_code != 200:
+                print('未下载成功：', url)
+                continue
+        except Exception as e:
+            print('未下载成功：', url)
+
+        filename = os.path.join('imgs', str(index) + '.jpg')
+        with open(filename, 'wb') as f:
+            f.write(res.content)
+            print('下载完成\n')
+            index += 1
+        print("下载结束，一共下载了 %s 张图片" % (index - 1))
+
+if __name__ == '__main__':
+    main()
---
+++ model.py
@@ -0,0 +1,9 @@
+#! usr/bin/python
+# coding=utf-8
+
+# 单个图片信息
+class ImgInfo:
+    def __init__(self, name,url,parentUrl):
+        self.name = name
+        self.url = url
+        self.parentUrl = parentUrl
